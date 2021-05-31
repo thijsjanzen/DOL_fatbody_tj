7d325d16da82887dd6ae853984e12d5f3944d10d
dewuem <dewuem1988@googlemail.com>
2021-05-31 10:31:32 +0200

    Initial Commit

---
+++ DOL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DOL
+//
+//  Created by Daniel Elsner on 31.05.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
