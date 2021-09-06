e0ae639c89771f97af95f931bedb080e3006e520
Thijs Janzen <thijsjanzen@gmail.com>
2021-09-06 13:06:48 +0200

    Update main_test.cpp

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -31,7 +31,20 @@ TEST_CASE("TEST individual") {
   test_indiv.update_fatbody(1.f);
   CHECK(test_indiv.get_fat_body() < 1.f);
 
+  test_indiv.reduce_crop(0.5f);
+  CHECK(test_indiv.get_crop() == 2.f);
 
+  test_indiv.eat_crop(1.f);
+  CHECK(test_indiv.get_crop() == 0.f);
+  CHECK(test_indiv.get_fat_body() == 1.f);
 
+  float new_food = test_indiv.handle_food(1.f, // food
+                         0.5f, // conversion_rate
+                         2.f,  // max_fat_body
+                         2,    // t
+                         0.5); // handling_time
 
+  CHECK(new_food == 0.0f); // remaining food is discarded
+  CHECK(test_indiv.get_fat_body() == 1.5f);
+  CHECK(test_indiv.get_next_t() == 2.5f);
 }
