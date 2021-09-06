b5a6e973c112d2c5c23471c12d4356e0020f9acd
Thijs Janzen <thijsjanzen@gmail.com>
2021-09-06 13:00:15 +0200

    expand testing

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -9,7 +9,14 @@
 #include <string>
 
 TEST_CASE("TEST individual") {
+
+  sim_param params;
+  rnd_t rndgen(42);
+  int id = 0;
+
   individual test_indiv;
+  test_indiv.set_params(params.get_ind_param(),
+                        id, rndgen);
   test_indiv.new_next_t(5.0);
   REQUIRE(test_indiv.get_next_t() == 5.0);
 
@@ -18,4 +25,13 @@ TEST_CASE("TEST individual") {
   test_indiv.process_crop(0.5f, 1.f);
   CHECK(test_indiv.get_crop() == 2.5f);
   CHECK(test_indiv.get_fat_body() == 1.f);
+
+  // check on update fatbody
+  //fat body before  = 1.0f
+  test_indiv.update_fatbody(1.f);
+  CHECK(test_indiv.get_fat_body() < 1.f);
+
+
+
+
 }
