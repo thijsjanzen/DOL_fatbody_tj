fc572613438056e7658ca580875a5798bae20792
Thijs Janzen <thijsjanzen@gmail.com>
2021-09-06 12:53:48 +0200

    update main_test.cpp

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -12,4 +12,10 @@ TEST_CASE("TEST individual") {
   individual test_indiv;
   test_indiv.new_next_t(5.0);
   REQUIRE(test_indiv.get_next_t() == 5.0);
+
+  test_indiv.set_fat_body(0.f);
+  test_indiv.set_crop(5.f);
+  test_indiv.process_crop(0.5f, 1.f);
+  CHECK(test_indiv.get_crop() == 2.5f);
+  CHECK(test_indiv.get_fat_body() == 1.f);
 }
